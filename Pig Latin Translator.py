3ab8a5e50f015289fdb2ce7addab44a6ff5f8878
Dan Wu <zhu.amy99@gmail.com>
2024-08-05 15:30:39 -0700

    Update Pig Latin Translator.py

--- Pig Latin Translator.py
+++ Pig Latin Translator.py
@@ -3,7 +3,7 @@ from decode import decode
 
 if __name__ == "__main__":
     while True:
-        dENcoder: str = input('Would you like to encode or decode Pig Latin?\nEnter 1 To encode\nEnter 2 to decode\n')
+        dENcoder: str = input('Would you like to encode or decode Pig Latin?\nEnter 1 to encode\nEnter 2 to decode\n')
         if dENcoder == '1':
             print("\n", encode_sentence(input('> ')))
         elif dENcoder == '2':
